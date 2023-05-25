2b2cf5b48920f664f16b43e63b13f6c77ced18b1
Terror-1 <elfarhosam487@gmail.com>
2023-05-25 22:18:07 +0200

    create dataloader for emaki

--- src/pytorch_converter.py
+++ src/pytorch_converter.py
@@ -2,10 +2,10 @@ import torch
 import opacus
 from opacus import PrivacyEngine
 import pandas as pd
-
+from EMAKI_DataLoader import CustomEMAKIDataset
 EMAKI_PATH = "../datasets/sample.pkl"
-dataset = pd.read_pickle(EMAKI_PATH)
-print(dataset)
+dataset = CustomEMAKIDataset()
+#print(dataset)
 model = torch.nn.Sequential(
     torch.nn.Linear(2, 2),
     torch.nn.Sigmoid(),
@@ -13,17 +13,16 @@ model = torch.nn.Sequential(
     torch.nn.Sigmoid()
 )
 
-
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 loss_fn = torch.nn.MSELoss()
 
 data_loader = torch.utils.data.DataLoader(dataset, batch_size=1024)
 
 privacy_engine = PrivacyEngine()
-"""model, optimizer, data_loader = privacy_engine.make_private(
+model, optimizer, data_loader = privacy_engine.make_private(
     module=model,
     optimizer=optimizer,
     data_loader=data_loader,
     noise_multiplier=1.1,
     max_grad_norm=1.0,
-) """
+) 
